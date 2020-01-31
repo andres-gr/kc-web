e04e2aadf15f6942f07bb21c93f30d0f577fb563
Andres Garcia <andres.gar@gmail.com>
2020-01-31 11:49:03 -0600

    format fixes

--- environments/frontend/webpack.config.js
+++ environments/frontend/webpack.config.js
@@ -4,7 +4,11 @@ const configValidations = require('./webpack/configValidations');
 
 module.exports = env => {
   if (!env) throw new Error(configValidations.ERR_NO_ENV_FLAG);
-  const isDev = ['development', 'staging', 'localhost'].includes(env);
+  const isDev = [
+    'development',
+    'localhost',
+    'staging',
+  ].includes(env);
   const configFile = isDev ? 'dev' : 'prod';
   const commonConfig = require('./webpack/webpack.common');
   const envConfig = require(`./webpack/webpack.${configFile}`);
