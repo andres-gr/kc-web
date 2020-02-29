685d6f04fbb062a7dc2f969e7b44d560a4fa9f2f
Andres Garcia <andres.gar@gmail.com>
2020-02-29 04:56:17 -0600

    changed openapi docs to use yaml file

--- environments/backend/src/routes/swagger.ts
+++ environments/backend/src/routes/swagger.ts
@@ -1,31 +1,12 @@
+import path from 'path'
 import {
   serve,
   setup,
 } from 'swagger-ui-express'
-import swaggerJSDoc from 'swagger-jsdoc'
+import yaml from 'yamljs'
 import router from 'Routes/router'
-import apis from 'Utils/swaggerApis'
 
-const port = process.env.PORT
-
-const options: swaggerJSDoc.Options = {
-  apis,
-  definition: {
-    info: {
-      description : 'Test project for API documentation and codegen',
-      title       : 'KC Web backend API',
-      version     : '1.0.0',
-    },
-    openapi : '3.0.0',
-    servers : [
-      {
-        url: `http://localhost:${port}/api/v1`,
-      },
-    ],
-  },
-}
-
-const specs = swaggerJSDoc(options)
+const specs = yaml.load(path.resolve(__dirname, '../api-v1.yaml'))
 
 router.use('/docs', serve)
 
@@ -39,7 +20,7 @@ router.get('/docs', setup(
 router.get('/api-docs', (_req, res) => {
   res.setHeader(
     'Content-Type',
-    'application/json',
+    'application/yaml',
   )
 
   res
